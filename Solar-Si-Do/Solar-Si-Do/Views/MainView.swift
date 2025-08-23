e1d5c379f3cd38fee1f4dfbbb3574dd22052d1b9
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-08-24 03:56:53 +0900

    fix: AddFeedbackView 레이아웃

--- Solar-Si-Do/Solar-Si-Do/Views/MainView.swift
+++ Solar-Si-Do/Solar-Si-Do/Views/MainView.swift
@@ -1,22 +1,27 @@
-
 import SwiftUI
 
 struct MainView: View {
     @StateObject private var viewModel = MainViewModel()
-    
+
     var body: some View {
-        TabView {
-            AddFeedbackView()
-                .tabItem {
-                    Text("피드백 주기")
-                }
-            
-            ReceiveFeedbackView()
-                .tabItem {
-                    Text("피드백 받기")
-                }
+        VStack(spacing: 0) {
+            TabView {
+                AddFeedbackView()
+                    .tabItem {
+                        Image(systemName: "person.crop.rectangle.stack")
+                        Text("피드백 주기")
+                    }
+
+                ReceiveFeedbackView()
+                    .tabItem {
+                        Image(systemName: "list.bullet")
+                        Text("피드백 받기")
+                    }
+            }
+            .tint(.black)
         }
     }
+
 }
 
 #Preview {
