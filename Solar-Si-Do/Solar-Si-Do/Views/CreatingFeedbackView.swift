d6fce73c5131097b580ecd797b26ab07308d7bd3
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-08-24 09:45:57 +0900

    feat: CreatingFeedback 뷰 구현

---
+++ Solar-Si-Do/Solar-Si-Do/Views/CreatingFeedbackView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct CreatingFeedbackView: View {
+    @StateObject private var viewModel = MainViewModel()
+
+    var body: some View {
+        ZStack {
+//            Color("sendwichYellow")
+//                .ignoresSafeArea()
+
+            VStack(spacing: 0) {
+                Text("피드백 샌드위치 조리중...")
+                    .font(.system(size: 22, weight: .bold))
+                    .padding(.top, 68)
+                    .padding(.horizontal, 20)
+
+                Text("너가 말해준 걸 토대로 피드백을 만들고 있어")
+                    .font(.system(size: 14, weight: .medium))
+                    .padding(.top, 8)
+                    .padding(.horizontal, 20)
+
+                LottieView(animationName: "Playful 3D Mascot Icon")  // 파일 이름에서 확장자 제거
+                    .frame(width: 324, height: 324)
+                    .padding(.top, 119)
+
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    CreatingFeedbackView()
+}
